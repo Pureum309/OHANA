2757a8864b91da83671a969474259fc3da35f496
DesmandD3 <dking68@my.bcit.ca>
2022-10-09 20:39:32 -0700

    map fixed

--- comps/Map.js
+++ comps/Map.js
@@ -62,7 +62,7 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: '#fff',
         alignItems: 'center',
-        justifyContent: 'center',
+        // justifyContent: 'center',
     },
     map: {
         width: Dimensions.get('window').width,
