4fb3372ea30ab4484ddc3bbac72425f642c77515
DesmandD3 <dking68@my.bcit.ca>
2022-10-16 21:08:09 -0700

    tab view not working

--- comps/User_Profile_Lower/user_lower.js
+++ comps/User_Profile_Lower/user_lower.js
@@ -1,16 +1,64 @@
-import React from "react";
-import { Tab, TabBar } from "@ui-kitten/components";
+// import React from 'react';
+// import { TabView, SceneMap } from 'react-native-tab-view';
+
+
+// export default function UserLower() {
+
+//     return (
+//         <TabView />
+//     );
+// }
+
+// import * as React from 'react';
+// import { View, useWindowDimensions } from 'react-native';
+// import { TabView, SceneMap } from 'react-native-tab-view';
+
+// const FirstRoute = () => (
+//     <View style={{ flex: 1, backgroundColor: '#ff4081' }} />
+// );
+
+// const SecondRoute = () => (
+//     <View style={{ flex: 1, backgroundColor: '#673ab7' }} />
+// );
+
+// const renderScene = SceneMap({
+//     first: FirstRoute,
+//     second: SecondRoute,
+// });
+
+// export default function UserLower() {
+//     const layout = useWindowDimensions();
+
+//     const [index, setIndex] = React.useState(0);
+//     const [routes] = React.useState([
+//         { key: 'first', title: 'First' },
+//         { key: 'second', title: 'Second' },
+//     ]);
+
+//     return (
+//         <TabView
+//             navigationState={{ index, routes }}
+//             renderScene={renderScene}
+//             onIndexChange={setIndex}
+//             initialLayout={{ width: layout.width }}
+//         />
+//     );
+// }
+
+import React from 'react';
+import { Tab, TabBar } from '@ui-kitten/components';
+import { View, Text } from 'react-native'
 
 export default function UserLower() {
 
-    //const [selectedIndex, setSelectedIndex] = React.useState(0);
-    // selectedIndex={selectedIndex}
-    //         onSelect={index => setSelectedIndex(index)}
-
     return (
-        <TabBar>
-            <Tab title='USERS' />
-            <Tab title='ORDERS' />
-        </TabBar>
+        <View>
+            <Text>This dumb thing even working?</Text>
+            <TabBar>
+                <Tab title='USERS' />
+                <Tab title='ORDERS' />
+                <Tab title='TRANSACTIONS' />
+            </TabBar>
+        </View>
     );
-}
+};
--- screen/Profile.js
+++ screen/Profile.js
@@ -2,16 +2,16 @@ import React from "react";
 import { View, Text } from 'react-native'
 import UserLower from "../comps/User_Profile_Lower/user_lower";
 
-import Header from "../comps/Header";
+// import Header from "../comps/Header";
 
 const Profile = (props) => {
     return (
-        // <View>
-        //     <Text>This is Profile</Text>
-        // </View>
-        <div>
+        <View>
+            <Text>This is Profile</Text>
+
+
             <UserLower />
-        </div>
+        </View>
     );
 }
 
