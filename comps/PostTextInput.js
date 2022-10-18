6e2824c34d590bdfd1b3a33b9a4961e04b6f1d15
DesmandD3 <dking68@my.bcit.ca>
2022-10-18 15:39:07 -0700

    styled field

--- comps/PostTextInput.js
+++ comps/PostTextInput.js
@@ -1,4 +1,4 @@
-import { View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import { TextInput } from 'react-native-paper';
 
 
@@ -8,8 +8,16 @@ export default function PostInput() {
             <TextInput
                 label="Tap to share what you are up to..."
                 right={<TextInput.Icon icon="microphone" />}
+                style={styles}
             >
             </TextInput>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    height: 190,
+    margin: 20,
+    borderRadius: 16,
+    backgroundColor: "#FFFFFF"
+});
