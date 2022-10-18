f719ee17626032bcbb9883fcad96f99f9927249d
Pureum309 <psun27@my.bcit.ca>
2022-10-17 21:52:11 -0700

    create dropdown menu

---
+++ comps/Dropdown/data.js
@@ -0,0 +1,16 @@
+export const category_data = [
+    { label: 'Grocery Pickup', value: '1' },
+    { label: 'Pharmacy Pickup', value: '2' },
+    { label: 'Post Office Dropoff', value: '3' },
+    { label: 'Post Office Pickup', value: '4' },
+    { label: 'Item 5', value: '5' },
+    { label: 'Item 6', value: '6' },
+    { label: 'Item 7', value: '7' },
+    { label: 'Item 8', value: '8' },
+];
+
+export const location_data = [
+    { label: 'Vancouver', value: '1' },
+    { label: 'New Westminster', value: '2' },
+
+]
--- screen/Post.js
+++ screen/Post.js
@@ -1,12 +1,18 @@
 import React from "react";
 import { View, Text } from 'react-native'
+import DropdownComponent from "../comps/Dropdown/Dropdown";
 
 import Header from "../comps/Header";
+
+import { category_data, location_data } from '../comps/Dropdown/data';
 
 const Post = (props) => {
     return (
         <View>
             <Text>This is Profile</Text>
+            <DropdownComponent label_txt="Category" data={category_data} />
+            <DropdownComponent label_txt="Location" data={location_data} />
+
         </View>
     )
 }
