c9e5db8b34b7726d7ab9229446593aaebbd1df36
DesmandD3 <dking68@my.bcit.ca>
2022-10-20 06:15:32 -0700

    added text field return

--- comps/PostTextInput.js
+++ comps/PostTextInput.js
@@ -1,16 +1,23 @@
+import React from 'react';
 import { View, StyleSheet } from 'react-native';
 import { TextInput } from 'react-native-paper';
 
 
 export default function PostInput() {
+    // "text" stores what the user types inside the field
+    const [text, setText] = React.useState("");
+
+    // testing output of "text"
+    console.log(text);
     return (
         <View>
             <TextInput
+                value={text}
                 label="Tap to share what you are up to..."
                 right={<TextInput.Icon icon="microphone" />}
                 style={styles}
-            >
-            </TextInput>
+                onChangeText={text => setText(text)}
+            />
         </View>
     )
 }
