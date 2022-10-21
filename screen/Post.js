feb6eda82f88609b599ae160f32c67972c6de7bc
bhail300 <bhaile300@gmail.com>
2022-10-21 00:19:12 -0700

    added popup comp to post page

--- screen/Post.js
+++ screen/Post.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { View, Text, SafeAreaView, ScrollView } from 'react-native'
 import DropMenuComp from "../comps/DropMenuComp";
+import Popup from "../comps/Popup";
 
 const Post = () => {
 
@@ -10,10 +11,13 @@ const Post = () => {
                 <View>
                     <Text>This is Profile</Text>
                     <DropMenuComp />
+                    <Popup/>               
                 </View>
             </ScrollView>
         </SafeAreaView>
     )
 }
 
-export default Post
+export default Post 
+
+/*<DropMenuComp />*/
