7325155bf546e1e48fee983bffd0a4350a769542
bhail300 <bhaile300@gmail.com>
2022-10-29 19:43:12 -0700

    put login comp on login screen

---
+++ screen/LogInScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View} from 'react-native';
+import Login from '../comps/Login/Login'
+
+const LoginScreen = () => {
+    return (
+        <View>
+            <Login/>
+        </View>
+    )
+}
+
+export default LoginScreen
