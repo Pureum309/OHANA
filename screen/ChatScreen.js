3f09685bc3aa65a5ba6a16063927793838b0ff47
soleildagneau <sdagneau@my.bcit.ca>
2022-11-07 12:09:47 -0800

    added back button to chat page

--- screen/ChatScreen.js
+++ screen/ChatScreen.js
@@ -1,13 +1,26 @@
 import React from "react";
 import { StyleSheet, Text, View, StatusBar, ScrollView, SafeAreaView, Dimensions } from 'react-native'
+import CustomButton from '../../comps/Login/CustomButton';
+import IonicIcon from 'react-native-vector-icons/Ionicons';
 
 import Chat from "../comps/Chat";
 
+
+
 const ChatScreen = (props) => {
 
+    const handleBack = () => {
+        console.log("test")
+        navigation.navigate('Home')
+    }
     return (
+
         <SafeAreaView>
             <ScrollView>
+                <View style={styles.back_button}>
+                    <IonicIcon name="arrow-back-outline" size={30} color="#00ADC3" />
+                    <CustomButton text="Go Back" onPress={handleBack} type="QUATERNARY" />
+                </View>
                 <View style={styles.container}>
                     <Chat />
                 </View>
@@ -23,6 +36,14 @@ const styles = StyleSheet.create({
         backgroundColor: '#fff',
         height: deviceHeight,
     },
+    back_button: {
+        flexDirection: 'row',
+        position: 'absolute',
+        top: 45,
+        left: 10,
+        alignItems: 'center',
+        paddingTop: 10,
+    }
 });
 
 export default ChatScreen
