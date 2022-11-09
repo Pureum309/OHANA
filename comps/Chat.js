9bc04d9843fe7631f453ae9406608e3c4fbef422
soleildagneau <sdagneau@my.bcit.ca>
2022-11-08 22:00:14 -0800

    styling & added search bar

--- comps/Chat.js
+++ comps/Chat.js
@@ -1,17 +1,80 @@
-import React from "react";
-import { View, Text } from 'react-native'
+import React, { useState } from "react";
+import { View, Text, FlatList, TextInput, StyleSheet, Dimensions } from 'react-native'
 
 import UserCard from "./Chat_User/UserCard";
 
 import { user_txts } from "./Chat_User/data"
 
+//DATABASE for FIRBASE
+import { loginUser } from "./Login/Login";
+import { db } from '../firebase/firebase';
+import { collection, onSnapshot, query, where } from "firebase/firestore";
+import Search from "./SearchBar";
+
+
 const Chat = (props) => {
+    const [relationships, setRelationships] = useState([]);
+    const [filter, setFilter] = useState("");
+
+    const onChangeText = (e) => {
+        setRelationships([]);
+        setFilter(e);
+    }
+
+    if (relationships.length == 0) {
+        const relRef = collection(db, `users/${loginUser.user.uid}/relationships`);
+        const unsubscribe = onSnapshot(relRef, (snapshot) => {
+            var tempRels = [];
+            snapshot.forEach((doc) => {
+                tempRels.push(doc.data());
+            });
+
+            if (filter.length != 0) {
+                tempRels = tempRels.filter((user) =>
+                    user.first.toLowerCase().includes(filter.toLowerCase()) ||
+                    user.last.toLowerCase().includes(filter.toLowerCase())
+                );
+            }
+
+            console.log(tempRels);
+
+            if (tempRels.length != 0)
+                setRelationships(tempRels);
+        });
+    }
+
+    let index = 0;
+
+    // console.log(relationships.filter(relationships => relationships.first.toLowerCase().includes("aa")));
     return (
         <View>
+            <TextInput
+                style={styles.input}
+                placeholder="Search"
+                onChangeText={onChangeText}
+            />
             {/* <UserCard name="Sun" rel="dd" /> */}
-            {user_txts.map(users => { return <UserCard name={users.name} rel={users.rel} pic={users.pic} /> })}
+            {relationships.map(users => { return <UserCard key={users.uid} name={[users.first, ' ', users.last]} rel={users.relationship} pic={user_txts[index++].pic} /> })}
+
         </View>
     )
 }
 
 export default Chat
+const deviceWidth = Dimensions.get('window').width;
+
+const styles = StyleSheet.create({
+    input: {
+        height: 50,
+        width: deviceWidth,
+        // margin: 12,
+        padding: 10,
+
+        borderColor: 'grey',
+        borderWidth: 1,
+        shadowColor: '#171717',
+        shadowOffset: { width: 3, height: 4 },
+        shadowOpacity: 0.2,
+        shadowRadius: 3,
+    },
+});
